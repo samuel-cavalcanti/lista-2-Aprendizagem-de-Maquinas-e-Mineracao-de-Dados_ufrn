c2e56fe61f4611e6a04360228cf1b3b9de74b1d2
samuel-cavalcanti <scavalcanti111@gmail.com>
2019-05-15 01:51:02 -0300

    update graphs

--- Questao2.py
+++ Questao2.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as pyplot
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
+
 def plot_function(x: np.array, y: np.array, fig_name: str, y_true=None) -> None:
     pyplot.figure(fig_name)
     pyplot.title(fig_name + " function")
@@ -15,7 +16,10 @@ def plot_function(x: np.array, y: np.array, fig_name: str, y_true=None) -> None:
         pyplot.legend()
 
     else:
-        pyplot.plot(x, y, "o")
+        pyplot.plot(x, y, "o", label="Y_pred")
+        if y_true is not None:
+            pyplot.plot(x, y_true, "or", label="Y_true",markersize=1.5)
+            pyplot.legend()
 
     pyplot.savefig("graficos/" + fig_name, format="png")
     pyplot.show()
@@ -32,6 +36,7 @@ def plot_training(label_train: str, label_val: str, title: str, y_label: str, tr
     pyplot.legend()
     pyplot.savefig("graficos/" + title, format="png")
     pyplot.show()
+
 
 def build_model_classifier(input_size: int, n_nodes: int) -> keras.models.Model:
     input_layer = keras.layers.Input(shape=(input_size,))
@@ -50,6 +55,7 @@ def logic(x_1: np.array):
 
     return np.array(y)
 
+
 def data_logic(n_samples: int) -> (np.array, np.array):
     x = np.array(
         [np.random.randint(0, 2, n_samples),
@@ -58,6 +64,7 @@ def data_logic(n_samples: int) -> (np.array, np.array):
     y = logic(x)
 
     return x, y
+
 
 def pred_logic_function() -> None:
     n_epochs = 40
@@ -77,6 +84,7 @@ def pred_logic_function() -> None:
     plot_training("Training acc", "Validation acc", "Logic Function accuracy", "Accuracy", acc, val_acc,
                   range(1, n_epochs + 1))
 
+
 '''
  2.ii
 '''
@@ -87,6 +95,7 @@ def g_x_data(n_samples: int) -> (np.array, np.array):
     x = np.random.uniform(1e-9, 4 * np.pi, n_samples)
     return x, g_x(x)
 
+
 def build_model_regression(input_size: int, n_nodes: int) -> keras.models.Model:
     input_layer = keras.layers.Input(shape=(input_size,))
     first_hidden_layer = keras.layers.Dense(n_nodes, activation=keras.activations.relu)(input_layer)
@@ -95,6 +104,7 @@ def build_model_regression(input_size: int, n_nodes: int) -> keras.models.Model:
     model = keras.models.Model(inputs=input_layer, outputs=output_layer)
     model.compile(optimizer="rmsprop", loss=keras.losses.mse, metrics=[keras.metrics.mae])
     return model
+
 
 def pred_g_x() -> None:
     n_epochs = 100
@@ -111,13 +121,13 @@ def pred_g_x() -> None:
 
     plot_training("Training loss", "Validation loss", "g(x) loss", "Loss", loss, val_loss,
                   range(1, n_epochs + 1))
-    plot_training("Training MAE", "Validation MSE", "g(x) MAE", "MAE", mean_absolute_error,
+    plot_training("Training MAE", "Validation MAE", "g(x) MAE", "MAE", mean_absolute_error,
                   val_mean_absolute_error,
                   range(1, n_epochs + 1))
     x_plot, y_plot = g_x_data(1000)
     y_pred = model.predict(x_plot)
 
-    plot_function(x_plot, y_pred, "Model Pred")
+    plot_function(x_plot, y_pred, "ModelPred_g(x)", y_pred)
 
 
 '''
@@ -155,6 +165,7 @@ def pred_h_x() -> None:
     x_plot, y_plot = h_x_data(1000)
     y_pred = model.predict(x_plot)
     plot_function(x_plot.T, y_pred, "ModelPred_h(x)", y_plot)
+
 
 if __name__ == '__main__':
     pred_logic_function()
