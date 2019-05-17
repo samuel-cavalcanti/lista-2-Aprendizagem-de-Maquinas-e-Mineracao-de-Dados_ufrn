132f475d220553902dfcd6b3973cb91afdc393b2
samuel-cavalcanti <scavalcanti111@gmail.com>
2019-05-17 19:23:32 -0300

    resilvido a Questão 4

--- Questao4.py
+++ Questao4.py
@@ -1,12 +1,22 @@
 import keras
 import matplotlib.pyplot as pyplot
 import numpy as np
+import cv2
 
 
 def test_load_dataset():
     (train_images, train_labels), (test_images, test_labels) = keras.datasets.mnist.load_data()
-    pyplot.imshow(train_images[0])
-    pyplot.show()
+    cv2.namedWindow("number", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("rotate number", cv2.WINDOW_NORMAL)
+    cv2.imshow("number", train_images[0])
+    rows, cols = train_images[0].shape
+
+    rotation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), np.random.uniform(0, 10), 1)
+    new_image = cv2.warpAffine(train_images[0], rotation_matrix, (cols, rows))
+    cv2.imshow("rotate number", new_image)
+    cv2.waitKey(0)
+    # pyplot.imshow(train_images[0])
+    # pyplot.show()
 
 
 def new_shape_for_keras(old_shape):
@@ -15,10 +25,19 @@ def new_shape_for_keras(old_shape):
     return tuple(new_shape)
 
 
+def rotate_image(images: np.array) -> np.array:
+    for i, image in enumerate(images):
+        rows, cols = image.shape
+        rotation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), np.random.uniform(0, 10), 1)
+        images[i] = cv2.warpAffine(image, rotation_matrix, (cols, rows))
+
+    return images
+
+
 def pre_processing(train_images: np.array, train_labels: np.array, test_images: np.array, test_labels: np.array) \
         -> (np.array, np.array, np.array, np.array):
-    x_train = train_images.reshape(new_shape_for_keras(train_images.shape))
-    x_test = test_images.reshape(new_shape_for_keras(test_images.shape))
+    x_train = rotate_image(train_images).reshape(new_shape_for_keras(train_images.shape))
+    x_test = rotate_image(test_images).reshape(new_shape_for_keras(test_images.shape))
     y_train = keras.utils.to_categorical(train_labels)
     y_test = keras.utils.to_categorical(test_labels)
 
@@ -67,11 +86,12 @@ def plot_history(history) -> None:
     plot_training("Training acc", "Validation acc", "Training and validation accuracy MNIST dataset", "Accuracy",
                   acc, val_acc, epochs)
 
-#TODO girar imagens e verifcar a classificação
-#https://towardsdatascience.com/building-a-convolutional-neural-network-cnn-in-keras-329fbbadc5f5
+
+# https://towardsdatascience.com/building-a-convolutional-neural-network-cnn-in-keras-329fbbadc5f5
 if __name__ == '__main__':
     x_train, y_train, x_test, y_test = load_dataset()
+
     model = build_model(tuple([x_test.shape[1], x_test.shape[2], x_test.shape[-1]]), y_train.shape[-1])
     history = model.fit(x_train, y_train, batch_size=50, epochs=3, validation_data=(x_test, y_test))
     plot_history(history.history)
-    model.save("convNN")# val_acc = 0.973
+    # model.save("convNN")# val_acc = 0.973
