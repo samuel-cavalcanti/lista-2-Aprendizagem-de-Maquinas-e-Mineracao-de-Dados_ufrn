a74cd4a263cf47db42902dd66c41749be7597f81
samuel-cavalcanti <scavalcanti111@gmail.com>
2019-05-22 02:06:44 -0300

    melhorado o desempenho da conv

--- Questao4.py
+++ Questao4.py
@@ -37,8 +37,15 @@ def rotate_image(images: np.array) -> np.array:
 
 def pre_processing(train_images: np.array, train_labels: np.array, test_images: np.array, test_labels: np.array) \
         -> (np.array, np.array, np.array, np.array):
-    x_train = rotate_image(train_images).reshape(new_shape_for_keras(train_images.shape))
-    x_test = rotate_image(test_images).reshape(new_shape_for_keras(test_images.shape))
+    # x_train = rotate_image(train_images).reshape(new_shape_for_keras(train_images.shape))
+    # x_test = rotate_image(test_images).reshape(new_shape_for_keras(test_images.shape))
+    #
+    # x_test = x_test.astype("float32") / 255
+    # x_train = x_train.astype("float32") / 255
+
+    x_train = (train_images.astype("float32") / 255).reshape(new_shape_for_keras(train_images.shape))
+    x_test = (test_images.astype("float32") / 255).reshape(new_shape_for_keras(test_images.shape))
+
     y_train = keras.utils.to_categorical(train_labels)
     y_test = keras.utils.to_categorical(test_labels)
 
@@ -53,10 +60,13 @@ def load_dataset() -> (np.array, np.array, np.array, np.array):
 def build_model(input_shape: tuple, output_size: int) -> keras.Sequential:
     model = keras.Sequential()
     model.add(keras.layers.Conv2D(64, kernel_size=3, activation=keras.activations.relu, input_shape=input_shape))
-    model.add(keras.layers.Conv2D(32, kernel_size=3, activation=keras.activations.relu))
+    model.add(keras.layers.MaxPool2D(pool_size=(2, 2)))
+    model.add(keras.layers.Dropout(0.25))
     model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(128, activation="relu"))
+    model.add(keras.layers.Dropout(0.5))
     model.add(keras.layers.Dense(output_size, activation=keras.activations.softmax))
-    model.compile(optimizer=keras.optimizers.adam(0.001), loss=keras.losses.categorical_crossentropy,
+    model.compile(optimizer=keras.optimizers.Adadelta(), loss=keras.losses.categorical_crossentropy,
                   metrics=["accuracy"])
     return model
 
@@ -88,18 +98,33 @@ def plot_history(history) -> None:
                   acc, val_acc, epochs)
 
 
-# https://towardsdatascience.com/building-a-convolutional-neural-network-cnn-in-keras-329fbbadc5f5
-if __name__ == '__main__':
-    x_train, y_train, x_test, y_test = load_dataset()
-    # print(y_train[0])
-    # model = build_model(tuple([x_test.shape[1], x_test.shape[2], x_test.shape[-1]]), y_train.shape[-1])
+def training_deep_learning(x_train: np.array, y_train: np.array, x_test: np.array,
+                           y_test: np.array) -> keras.Sequential:
+    model = build_model(tuple([x_test.shape[1], x_test.shape[2], x_test.shape[-1]]), y_train.shape[-1])
+    history = model.fit(x_train, y_train, epochs=12, validation_data=(x_test, y_test))
+    plot_history(history.history)
 
-    # history = model.fit(x_train, y_train, batch_size=50, epochs=3, validation_data=(x_test, y_test))
-    # plot_history(history.history)
-    model = keras.models.load_model("Redes_Salvas/Questao4_convNN")
+    return model
+
+
+def evaluate_deep_learning(model: keras.Sequential, x_test: np.array, y_test: np.array):
     all_pred = model.predict(x_test)
     y_pred = [str(np.argmax(pred)) for pred in all_pred]
     y_true = [str(np.argmax(i)) for i in y_test]
     matrix = metrics.confusion_matrix(y_true, y_pred, labels=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
     print(matrix)
-    # model.save("convNN")# val_acc = 0.973
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+
+# https://towardsdatascience.com/building-a-convolutional-neural-network-cnn-in-keras-329fbbadc5f5
+# https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
+
+if __name__ == '__main__':
+    x_train, y_train, x_test, y_test = load_dataset()
+
+    # model = training_deep_learning(x_train, y_train, x_test, y_test)
+    # model.save("Redes_Salvas/Questao4_convNN")
+    model = keras.models.load_model("Redes_Salvas/Questao4_convNN")
+    evaluate_deep_learning(model, x_test, y_test)
